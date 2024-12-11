b30b4464990f20113a953ad8b58cde8058db6351
Vladislav <tekuzar@mail,ru>
2024-12-11 21:31:38 +0300

    Done!

---
+++ src/main/java/com/example/homework28/EmployeeServiceInter.java
@@ -0,0 +1,13 @@
+package com.example.homework28;
+
+
+import java.util.List;
+
+public interface EmployeeServiceInter {
+
+    Employee getEmployeeMaxSalary (int department);
+    Employee getEmployeeMinSalary (int department);
+    List<Employee> getAllEmployeesByDep(int department);
+
+
+}
